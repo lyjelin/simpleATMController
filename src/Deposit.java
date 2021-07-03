affdf8babd4d1c15ffb2cb2705d96b20e15635ed
lyjelin <u3560393@connect.hku.hk>
2021-07-04 01:26:49 +0800

    Implemented Deposit function

--- src/Deposit.java
+++ src/Deposit.java
@@ -4,11 +4,49 @@ import java.security.*;
 public class Deposit extends Service {
 
     @Override
-    public void func(BufferedReader input, User u, int clientAccIndex, int clientNum)
-            throws IOException, NoSuchAlgorithmException {
+    public void func(BufferedReader input, User u, int clientAccIndex, int clientNum) throws IOException, NoSuchAlgorithmException {
         super.func(input, u, clientAccIndex, clientNum);
-
         
+        System.out.println("");
+        System.out.println("Enter amount for cash deposit : ");
+
+        String amount = input.readLine();
+
+        System.out.println("");
+        System.out.println("Amount entered is : "+amount);
+        System.out.println("Are you confirmed? Y/N");
+
+        String confirmation = input.readLine();
+        boolean flag = true;
+
+        while(flag){
+            if (confirmation == "Y" || confirmation == "y"){
+                int currentBalance = u.getAccBalance(clientNum).get(clientAccIndex);
+                u.getAccBalance(clientNum).set(clientAccIndex, currentBalance + Integer.parseInt(amount));
+                System.out.println("");
+                System.out.println(currentBalance + Integer.parseInt(amount)+" added into your account ["+u.getAcc().get(clientNum).get(clientAccIndex)+"] successfully");
+                System.out.println("");
+                System.out.println("Returning back to Transaction Main ...");
+                flag = false;
+            }
+            else if (confirmation == "N" || confirmation == "n"){
+                System.out.println("Cash Deposit Cancelled");
+                System.out.println("Returning back to Transaction Main ...");
+                flag = false;
+    
+            }
+            else {
+                System.out.println("Invalid input");
+                System.out.println("Please enter either Y or N");
+
+            }
+
+        }
+
+        return;
+
+
+
 
         
     }
