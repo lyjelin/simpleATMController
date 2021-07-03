c1821a23397ad475cd0db495efb59330cf4708ad
lyjelin <u3560393@connect.hku.hk>
2021-07-04 02:18:47 +0800

    Modify IO function

--- src/addRegisteredUser.java
+++ src/addRegisteredUser.java
@@ -1,107 +1,44 @@
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.io.*;
 
 public class addRegisteredUser extends HashAlgo {
 
     public addRegisteredUser(){}
 
-    public void database(User u) throws NoSuchAlgorithmException {
-        
-        /**
-         * User 1 - Bear
-         */
-        ArrayList<String> bearAcc = new ArrayList<String>();
-        ArrayList<Integer> bearAccBalance = new ArrayList<Integer>();
+    public void database(User u) throws NoSuchAlgorithmException, IOException {
 
-        bearAcc.add("0000-00-0000000");
-        bearAcc.add("0000-00-0000001");
-        bearAccBalance.add(100000);
-        bearAccBalance.add(200000);
+        File clientIntoFile = new File("clientDB.txt");
+        FileReader fr = new FileReader(clientIntoFile);
+        BufferedReader br = new BufferedReader(fr);
 
-        u.addName("BEAR");
-        u.addCardNum("000000");
-        u.addhashedPin(hashedPin("2017"));
-        u.addAccNum(bearAcc);
-        u.addAccBalance(bearAccBalance);
-        u.addLoginStatus(true);
-        u.addLoginFailCount(0);
+        String clientInfo;
 
-        /**
-         * User 2 - Elin
-         */
-        ArrayList<String> elinAcc = new ArrayList<String>();
-        ArrayList<Integer> elinAccBalance = new ArrayList<Integer>();
+        while ((clientInfo = br.readLine())!=null){
+            String[] tempArray = clientInfo.split(";");
 
-        elinAcc.add("0000-00-0000002");
-        elinAcc.add("0000-00-0000003");
-        elinAccBalance.add(210703);
-        elinAccBalance.add(999999);
+            ArrayList<String> acc= new ArrayList<String>();
+            ArrayList<Integer> accBalance= new ArrayList<Integer>();
 
-        u.addName("Elin");
-        u.addCardNum("000001");
-        u.addhashedPin(hashedPin("1999"));
-        u.addAccNum(elinAcc);
-        u.addAccBalance(elinAccBalance);
-        u.addLoginStatus(true);
-        u.addLoginFailCount(0);
+            u.addName(tempArray[0]);
+            u.addCardNum(tempArray[1]);
 
-        /**
-         * User 3 - Panda
-         */
-        ArrayList<String> pandaAcc = new ArrayList<String>();
-        ArrayList<Integer> pandaAccBalance = new ArrayList<Integer>();
+            String[] accHoldByClient = (tempArray[3]).split(",");
+            String[] eachAccBalance = (tempArray[4]).split(",");
+            if (accHoldByClient.length > 0){
+                for (int i=0;i<accHoldByClient.length;i++){
+                    acc.add(accHoldByClient[i]);
+                    accBalance.add(Integer.parseInt(eachAccBalance[i]));
+                }
+            }
+            u.addAccNum(acc);
+            u.addAccBalance(accBalance);
 
-        pandaAcc.add("0000-00-0000004");
-        pandaAcc.add("0000-00-0000005");
-        pandaAccBalance.add(623450);
-        pandaAccBalance.add(123905);
+            u.addLoginStatus(Boolean.parseBoolean(tempArray[5]));
+            u.addLoginFailCount(Integer.parseInt(tempArray[6]));
+        }
 
-        u.addName("Panda");
-        u.addCardNum("000002");
-        u.addhashedPin(hashedPin("6606"));
-        u.addAccNum(pandaAcc);
-        u.addAccBalance(pandaAccBalance);
-        u.addLoginStatus(true);
-        u.addLoginFailCount(0);
-        
-        /**
-         * User 4 - Polar
-         */
-        ArrayList<String> polarAcc = new ArrayList<String>();
-        ArrayList<Integer> polarAccBalance = new ArrayList<Integer>();
+        br.close();
 
-        polarAcc.add("0000-00-0000006");
-        polarAcc.add("0000-00-0000007");
-        polarAccBalance.add(829800);
-        polarAccBalance.add(456730);
-
-        u.addName("Polar");
-        u.addCardNum("000003");
-        u.addhashedPin(hashedPin("1234"));
-        u.addAccNum(polarAcc);
-        u.addAccBalance(polarAccBalance);
-        u.addLoginStatus(true);
-        u.addLoginFailCount(0);
-
-        /**
-         * User 5 - Pooh
-         */
-        ArrayList<String> poohAcc = new ArrayList<String>();
-        ArrayList<Integer> poohAccBalance = new ArrayList<Integer>();
-
-        poohAcc.add("0000-00-0000008");
-        poohAcc.add("0000-00-0000009");
-        poohAccBalance.add(500000);
-        poohAccBalance.add(750000);
-
-        u.addName("Pooh");
-        u.addCardNum("000004");
-        u.addhashedPin(hashedPin("3333"));
-        u.addAccNum(poohAcc);
-        u.addAccBalance(poohAccBalance);
-        u.addLoginStatus(true);
-        u.addLoginFailCount(0);
-        
     }
-    
 }
